5a1dbfaf16f8641c4abdf5406a52b47a41f81eee
Deanh94 <Dean.haim94@gmail.com>
2022-05-20 18:18:09 -0700

    update

---
+++ src/components/Stars.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import Products from './Products.js';
+
+export default function Stars() {
+  const [value, setValue] = React.useState(2);
+
+  return(
+    <Box
+    sx={{
+      '& > legend': { mt: 2 },
+    }}
+    >
+      <Typography component="legend"></Typography>
+      <Rating name="read-only" value={value} readOnly />
+    </Box>
+  );
+}
+
+
+
